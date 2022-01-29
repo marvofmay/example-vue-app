d4d0ce26f41edb3b45608d513fe4724fb6916584
Marcin <kontakt@internetowe-aplikacje.pl>
2022-01-29 11:51:25 +0100

    jest tests

---
+++ tests/unit/Component/welcome.spec.js
@@ -0,0 +1,22 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import Welcome from '@/components/Home/Welcome.vue'
+
+describe('Welcome.vue', () => {
+    
+  beforeEach(() => {
+    let wrapper = mount(Welcome)
+  });    
+    
+  it('renders Welcome.vue ', () => {
+    // const wrapper = mount(Welcome)
+    expect(wrapper.exists()).toBe(true)    
+  })    
+    
+  it('renders props.msg in Welcome.vue when passed', () => {
+    const msg = 'Witaj w przykładowej aplikacji Vue.js'
+    const wrapper = shallowMount(Welcome, {
+      props: { msg }
+    })
+    expect(wrapper.text()).toBe(msg)
+  })
+})
