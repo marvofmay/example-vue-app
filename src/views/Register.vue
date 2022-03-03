baca1e1347399b03813749cf75b32e66382e750f
Marcin <kontakt@internetowe-aplikacje.pl>
2022-03-03 01:18:48 +0100

    komentarz

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -22,7 +22,8 @@ export default {
     }
   },
   methods: {
-    register () {
+    // register
+    register () {      
       firebase
         .auth()
         .createUserWithEmailAndPassword(this.email, this.password)
